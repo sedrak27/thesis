3e2d83f2559fc9bd0b9947e97f93e9aeae040cdf
sedrak27 <sedrak.tsarukyan99@gmail.com>
2023-04-27 02:15:03 +0400

    added solution adding options and error handling

--- src/components/solutions/AddSolution.vue
+++ src/components/solutions/AddSolution.vue
@@ -6,41 +6,45 @@
                     <div class="card-body col-lg-12">
                         <h5 class="card-title">Ավելացնել խնդիր</h5>
                         <hr>
-                        <form @submit.prevent="changeProfileData" enctype="multipart/form-data">
-                            <div class="row">
+                        <form id="formData" enctype="multipart/form-data">
+                            <div class="row" ref="body">
+                                <div class="mb-3">
+                                    <label for="problem" class="form-label">Խնդիր</label>
+                                    <textarea class="form-control" id="problem" rows="3" ref="problem" name="problem"></textarea>
+                                </div>
 
-                                <div class="col-lg-8">
-                                    <div class="mb-3">
-                                        <label for="address" class="form-label">Խնդիր</label>
-                                        <textarea class="form-control" id="address" rows="3"></textarea>
-                                    </div>
+                                <div class="mb-3">
+                                    <label for="description" class="form-label">Լուծման նկարագրություն</label>
+                                    <textarea class="form-control" id="description" rows="3" ref="description" name="description"></textarea>
+                                </div>
 
-                                    <div class="mb-3">
-                                        <label for="address" class="form-label">Լուծման նկարագրություն</label>
-                                        <textarea class="form-control" id="address" rows="3"></textarea>
-                                    </div>
+                                <div class="mb-3">
+                                    <div ref="solutions"></div>
+                                    <button v-if="saveButton" class="btn btn-primary" type="button" @click="save">Պահել</button>
+                                </div>
 
-                                    <div class="mb-3">
-                                        <label for="avatar" class="form-label">Լուծում</label>
-                                        <input type="file" class="form-control" multiple @change="handleFileUpload">
-                                    </div>
-
-                                    <div class="col-lg-12 d-flex justify-content-end">
-                                        <button class="btn btn-primary" type="submit">Պահպանել</button>
+                                <div class="mb-3 d-flex flex-column">
+                                    <label class="form-label">Լուծում</label>
+                                    <div>
+                                        <button class="btn btn-primary" type="button" @click="addPicture">Նկար</button> &nbsp;
+                                        <button class="btn btn-primary" type="button" @click="addText">Տեքստ</button>
                                     </div>
                                 </div>
 
-                                <div class="col-lg-4">
-                                    <div class="images-parent col-lg-12">
-                                        <div v-for="(image, index) in images" :key="index" class="images">
-                                            <img :src="image.url" :alt="image.name">
-                                            <button class="remove-image" :value="index" @click="removeImage">X</button>
-                                        </div>
-                                    </div>
-                                    <button v-if="solution.length > 0" class="btn btn-primary mt-2" @click="clearImages">Մաքրել</button>
+                                <div class="col-lg-12 d-flex justify-content-end">
+                                    <button class="btn btn-primary" type="button" @click="saveSolutions">Պահպանել</button>
                                 </div>
 
                             </div>
+                            <!--                                <div class="col-lg-4">-->
+<!--                                    <div class="images-parent col-lg-12">-->
+<!--                                        <div v-for="(image, index) in solution" :key="index" class="images">-->
+<!--                                            <img :src="image.url" :alt="image.name">-->
+<!--                                            <button class="remove-image" :value="index" @click="removeImage">X</button>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                    <button v-if="solution.length > 0" class="btn btn-primary mt-2" @click="clearImages">Մաքրել</button>-->
+<!--                                </div>-->
                         </form>
                     </div>
                 </div>
@@ -59,42 +63,136 @@ export default {
         return {
             token: localStorage.getItem('jwtToken'),
             solution: [],
+            images: [],
+            currentSolution: 0,
+            saveButton: false,
+            solutionImage: null,
+            solutionDiv: null,
         }
     },
 
     methods: {
-        changeProfileData() {
-            axios.post('http://192.168.40.131:3000/solution/add', this.userData, { 'Authorization': `Bearer ${this.token}` })
-                .then(response => {
-                    this.userData = response.data;
-                })
-                .catch(error => {
-                    // console.log(error);
-                });
-        },
+        saveSolutions() {
+            if (!this.$refs.problem.value) {
+                this.$refs.problem.style = 'border: solid red 2px;';
+                this.$refs.problem.placeholder = 'Դաշտը պարտադիր լրացվող է';
 
-        handleFileUpload(event) {
-            const files = event.target.files;
-            for (let i = 0; i < files.length; i++) {
-                const file = files[i];
-                const reader = new FileReader();
-                reader.onload = (event) => {
-                    this.images.push({
-                        name: file.name,
-                        url: event.target.result,
+                return;
+            } else {
+                this.$refs.problem.style = 'border: solid green 2px;';
+            }
+
+            if (!this.$refs.description.value) {
+                this.$refs.description.style = 'border: solid red 2px;';
+                this.$refs.description.placeholder = 'Դաշտը պարտադիր լրացվող է';
+
+                return;
+            } else {
+                this.$refs.description.style = 'border: solid green 2px;';
+            }
+
+            if (!this.saveButton && this.$refs.solutions.hasChildNodes()) {
+                const form = document.getElementById("formData");
+                const formData = new FormData(form);
+
+                axios.post('http://192.168.40.131:3000/solution/add', formData, {'Authorization': `Bearer ${this.token}`})
+                    .then(response => {
+                        this.userData = response.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
                     });
-                };
-                reader.readAsDataURL(file);
+            } else {
+                alert('Լուծման դաշտը պարտադիր լրացվոող է');
             }
         },
 
-        removeImage(event) {
-            this.images.splice(event.target.value, 1);
+        addText() {
+            if (!this.saveButton) {
+                const div = document.createElement('div');
+                div.classList.add('mb-3');
+
+                const label = document.createElement('label');
+                label.classList.add('form-label');
+                label.textContent = ++this.currentSolution;
+
+                const input = document.createElement('input');
+                input.classList.add('form-control');
+                input.setAttribute('name', 'solution-' + this.currentSolution);
+
+                div.appendChild(label);
+                div.appendChild(input);
+
+                this.$refs.solutions.appendChild(div);
+
+                this.saveButton = true;
+            }
         },
 
-        clearImages() {
-            this.images = [];
-        }
+        addPicture() {
+            if (!this.saveButton) {
+                this.div = document.createElement('div');
+                this.div.classList.add('mb-3');
+
+                const label = document.createElement('label');
+                label.classList.add('form-label');
+                label.textContent = ++this.currentSolution;
+
+                const input = document.createElement('input');
+                input.classList.add('form-control');
+                input.setAttribute('name', 'solution-' + this.currentSolution);
+                input.setAttribute('type', 'file');
+                input.addEventListener('change', this.handlePictureUpload.bind(this))
+
+                this.div.appendChild(label);
+                this.div.appendChild(input);
+
+                this.$refs.solutions.appendChild(this.div);
+
+                this.saveButton = true;
+            }
+        },
+
+        save() {
+            const inputs = this.$refs.solutions.querySelectorAll('div div input');
+
+            for (const input of inputs) {
+                if (!input.value) {
+                    input.style = 'border: solid red 2px;';
+                    input.placeholder = 'Դաշտը պարտադիր լրացվող է';
+                    return;
+                }
+
+                input.disabled = true
+                input.style = 'border: solid green 2px;';
+            }
+
+            this.saveButton = false;
+        },
+
+        handlePictureUpload(event) {
+            const imageDiv = document.createElement('div');
+
+            this.solutionImage = document.createElement('img');
+            this.solutionImage.classList.add('mt-3');
+            this.solutionImage.setAttribute('width', '350px');
+            this.solutionImage.setAttribute('height', '250px');
+
+
+            const files = event.target.files;
+            const file = files[0];
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                this.images[this.currentSolution] = event.target.result;
+                this.solutionImage.setAttribute('src', this.images[this.currentSolution]);
+            };
+
+            reader.readAsDataURL(file);
+
+            imageDiv.appendChild(this.solutionImage);
+
+            this.div.appendChild(imageDiv);
+        },
     },
 }
 </script>
@@ -103,35 +201,6 @@ export default {
     .container {
         height: 75vh;
     }
-
-    .remove-image {
-        max-width: 50px;
-        max-height: 30px;
-        border: 1px black solid;
-        background-color: red;
-        border-radius: 50%;
-        position: absolute;
-        right: 10px;
-        top: 10px;
-        font-weight: bold;
-    }
-
-    .images-parent {
-        display: flex;
-        flex-wrap: wrap;
-    }
-
-    .images {
-        position: relative;
-        max-width: 200px;
-        max-height: 150px;
-    }
-
-    img {
-        max-width: 200px;
-        max-height: 150px;
-    }
-
     .card-body {
         overflow:scroll;
         height: 700px;
